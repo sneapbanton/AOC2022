bc7c9dc5bc808d47829e919bbaccbae83c523c61
Anton Eldeborg Lundin <antel460@student.liu.se>
2022-12-01 06:10:35 +0100

    Day one done

---
+++ 1/P1.py
@@ -0,0 +1,15 @@
+
+elfs = []
+data = open("In1.txt")
+prev = "\n"
+for line in data:
+    if prev == "\n" and line != "\n":
+        elfs.append(int(line))
+    elif line != "\n":
+        elfs[-1] = elfs[-1] + int(line)
+    prev = line
+
+elfs.sort()
+elfs.reverse()
+
+print(elfs[0])
