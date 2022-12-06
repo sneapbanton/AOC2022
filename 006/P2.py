b8a56eafcae8283c2325b4d8539cda4238bc84f0
Anton Eldeborg Lundin <antel460@student.liu.se>
2022-12-06 06:16:37 +0100

    Day 6 done, thoughts i was quick, but only gained 1 position :(

---
+++ 006/P2.py
@@ -0,0 +1,14 @@
+
+
+data = open("In2.txt")
+for line in data:
+    line = line[:-1]
+    matching = [line[0],line[1],line[2]]
+    for i in range(3, len(line)):
+        if len(matching) == 14:
+            matching.pop(0)
+        new_char = line[i]
+        matching.append(new_char)
+        if len(set(matching)) == 14:
+            print(i+1)
+            break
