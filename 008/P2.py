9ed8e364637849470004bcc874632d8ef2889c66
Anton <antel460@student.liu.se>
2022-12-08 13:29:38 +0100

    Day 8 part 2 done

--- 008/P2.py
+++ 008/P2.py
@@ -4,12 +4,18 @@ def recursererserr(data, grid, current_coord, rule, counter, highest_tree):
     new_x = current_coord[0] + rule[0]
     new_y = current_coord[1] + rule[1]
     if new_x < 0 or new_y < 0 or new_x >= len(grid[0]) or new_y >= len(grid):
-        return counter
-    elif grid[new_y][new_x] >= data or grid[new_y][new_x] < highest_tree:
-        return counter
-    return recursererserr(data, grid, (new_x,new_y), rule, counter+1, grid[new_y][new_x])
+        return counter - 1
+    else:
+        new_x = current_coord[0] + rule[0]
+        new_y = current_coord[1] + rule[1]
+        if grid[new_y][new_x] >= data:
+            return counter
+        elif grid[new_y][new_x] < highest_tree:
+            return recursererserr(data, grid, (new_x, new_y), rule, counter+1, highest_tree)
+        else:
+            return recursererserr(data, grid, (new_x,new_y), rule, counter+1, grid[new_y][new_x])
 
-data = open("test1.txt")
+data = open("In1.txt")
 grid = []
 for line in data:
     row = []
@@ -26,7 +32,5 @@ for y in range(1, len(grid)-1):
         factor3 = recursererserr(grid[y][x], grid, (x,y), (-1,0), 1, 0)
         factor4 = recursererserr(grid[y][x], grid, (x,y), (0,-1), 1, 0)
         factors.append(factor1*factor2*factor3*factor4)
-        
-
 
 print(max(factors))
