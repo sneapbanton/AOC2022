2dd6d180ee4d1f50b259dd6e59e95839ac8bcb83
Anton Eldeborg Lundin <antel460@student.liu.se>
2022-12-12 00:49:11 +0100

    day 11 ourrurhrhghghghg

---
+++ 011/P1.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+
+def alter(modifier, current, value):
+    if value == "old":
+        value = current
+    value = int(value)
+    if modifier == "+":
+        return current + value
+    else:
+        return current * value
+
+
+data = open("test1.txt")
+
+monkey = defaultdict(list)
+operations = []
+modifiers = []
+dividers = []
+true_monke = []
+false_monke = []
+inspected = [0] * 10
+current_monke = 0
+for line in data:
+
+    if line[:6] == "Monkey":
+        current_monke = int(line[7])
+    elif line[2:7] == "Start":
+        values = line[17:].split(" ")
+        for v in values[1:]:
+            if v[-1] == ",":
+                v = v[:-1]
+            monkey[current_monke].append(int(v))
+
+    elif line[2:7] == "Opera":
+        oper = line[23]
+        operations.append(oper)
+        modifiers.append(line[25:-1])
+
+    elif line[2:6] == "Test":
+        divider = int(line[2:].split(" ")[3])
+        dividers.append(int(divider))
+
+    elif line[4:8] == "If t":
+        to_monke = int(line[4:].split(" ")[-1])
+        true_monke.append(int(to_monke))
+
+    elif line[4:8] == "If f":
+        to_monke = int(line[4:].split(" ")[-1])
+        false_monke.append(int(to_monke))
+
+for i in range(20):
+    for monke in range(len(operations)):
+        for j in range(len(monkey[monke])):
+            inspected[monke] += 1
+            item = monkey[monke].pop(0)
+            item = int(alter(operations[monke], item, modifiers[monke]) / 3)
+            if item % dividers[monke] == 0:
+                monkey[true_monke[monke]].append(item)
+            else:
+                monkey[false_monke[monke]].append(item)
+
+inspected.sort(reverse=True)
+
+print(inspected[0]*inspected[1])
