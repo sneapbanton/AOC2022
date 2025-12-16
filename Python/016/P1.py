5d8c501e7098c320a9961f1163c7bc16f7e50b62
Anton Eldeborg Lundin <antel460@student.liu.se>
2025-12-16 15:31:57 +0100

    Update day 16 P1.py

--- Python/016/P1.py
+++ Python/016/P1.py
@@ -1,39 +1,41 @@
-import copy
-from collections import defaultdict
 
-data = open("test1.txt")
+data = open("In1.txt")
 flows = dict()
 tunnels = dict()
-dists = defaultdict(dict)
 for line in data:
     words = line.split(" ")
     name = words[1]
     flowrate = int(words[4][5:-1])
     leads_to = words[9:]
+    
     for i in range(len(leads_to)):
         if leads_to[i][-1] in [',', '\n']:
             leads_to[i] = leads_to[i][:-1]
     flows[name] = flowrate
     tunnels[name] = leads_to
 
-def highest_flow(opened):
-    summer = 0
-    for o in opened:
-        summer += flows[(o[0])]*int(o[1])
-    return summer
+from collections import defaultdict
+from functools import cache
 
-def bfs(start):
-    visited = []
-    queue = [(start, 0)]
-    while queue:
-        current, depth = queue.pop(0)
-        visited.append(current)
-        if current != start and flows[current]:
-            dists[start][current] = depth
-        for neigh in tunnels[current]:
-            if neigh not in visited:
-                queue.append((neigh, depth+1)) # type: ignore
+visited = defaultdict(lambda: -1)
+
+@cache
+def dfs(curr, flow, opened, time):
+
+    if visited[curr+str(flow)] != -1 and visited[curr+str(flow)] >= time:
+        return 0
+    if time==0:
+        return flow
+    vals = []
+    for neigh in tunnels[curr]:
+        if neigh not in opened and flows[neigh] and time > 2:
+            new_flow = flow + flows[neigh]*(time-2)
+            new_opened = list(opened)
+            new_opened.append(neigh)
+            vals.append(dfs(neigh, new_flow, tuple(new_opened), time-2))
+        vals.append(dfs(neigh, flow, opened, time-1))
+    visited[curr+str(flow)] = time
+    return max(vals)
 
 
-
-
+print(dfs("AA", 0, tuple([]), 30))
